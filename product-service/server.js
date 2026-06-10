8d86daeee346253a2f34128881a747313d3a3167
root <root@ip-172-31-89-99.ec2.internal>
2026-06-10 13:55:39 +0000

    Added Prometheus metrics endpoint

--- product-service/server.js
+++ product-service/server.js
@@ -1,18 +1,46 @@
 const express = require('express');
+const client = require('prom-client');
 
 const app = express();
 
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new client.Counter({
+  name: 'product_service_http_requests_total',
+  help: 'Total HTTP requests to product service',
+  labelNames: ['method', 'route', 'status']
+});
+
+register.registerMetric(httpRequestCounter);
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode
+    });
+  });
+  next();
+});
+
 app.get('/', (req, res) => {
-    res.send('Product Service Running - ArgoCD GitOps Demo');
+  res.send('Product Service Running - GitOps Success');
 });
 
 app.get('/products', (req, res) => {
-    res.json([
-        { id: 1, name: 'iPhone 15', price: 80000 },
-        { id: 2, name: 'Samsung S24', price: 70000 }
-    ]);
+  res.json([
+    { id: 1, name: 'iPhone 15', price: 80000 },
+    { id: 2, name: 'Samsung S24', price: 70000 }
+  ]);
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 app.listen(5000, () => {
-    console.log('Product Service running on port 5000');
+  console.log('Product Service running on port 5000');
 });
