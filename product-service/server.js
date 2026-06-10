7e32f9169264a596e9937f46975432e931dece2d
root <root@ip-172-31-89-99.ec2.internal>
2026-06-10 11:22:22 +0000

    Added product-service with Docker support

---
+++ product-service/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Product Service Running');
+});
+
+app.get('/products', (req, res) => {
+    res.json([
+        { id: 1, name: 'iPhone 15', price: 80000 },
+        { id: 2, name: 'Samsung S24', price: 70000 }
+    ]);
+});
+
+app.listen(5000, () => {
+    console.log('Product Service running on port 5000');
+});
